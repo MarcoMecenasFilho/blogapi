4e203fd5e2df53e3c9e12bb33c4027677f1f221e
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-03-14 10:50:04 -0300

    cria controller de buscar todos os posts

--- controllers/blogPostController.js
+++ controllers/blogPostController.js
@@ -23,8 +23,25 @@ const create = async (req, res, next) => {
   }
 };
 
+const getAll = async (_req, res, next) => {
+  try {
+    const allPosts = await BlogPost.findAll(
+      { include: [
+        { model: User, as: 'user', attributes: { exclude: ['password'] } },
+        { model: Category, as: 'categories', through: { attributes: [] } },
+      ] },
+    );
+
+    return res.status(200).json(allPosts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
 
 
 module.exports = {
   create,
+  getAll,
   };
