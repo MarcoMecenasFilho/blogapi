2da419c79a552b334ab3342ef60bc59b34f3cd7e
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-03-14 10:28:35 -0300

    cria controller de buscar user por id

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,25 @@ const getAll = async (_req, res, next) => {
   }
 };
 
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id, {
+      attributes: { exclude: ['password'] },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User does not exist' });
+    }
+
+    return res.status(200).json(user);
+  } catch (e) {
+    next(e);
+  }
+};
+
 module.exports = {
   create,
-  getAll
+  getAll,
+  getById
 };
