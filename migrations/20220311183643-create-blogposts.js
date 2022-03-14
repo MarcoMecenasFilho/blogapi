bd9f7be97c04af8d5aea8c68cf8dfb93c7513a1f
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-03-14 10:16:38 -0300

    cria migrate do blogposts

---
+++ migrations/20220311183643-create-blogposts.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+        
+      },
+      published: {
+        type: Sequelize.DATE
+      },
+      updated: {
+        type: Sequelize.DATE
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
