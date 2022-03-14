572e8f3ed357b096d6b5e982caf976654a81a56c
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-03-14 14:18:39 -0300

    deleta pasta middleware repetido

--- middlewares/middlewares/errorHandler.js
+++
@@ -1,4 +0,0 @@
-module.exports = (error, _req, res, _next) => {
-  if (error.status) return res.status(error.status).json({ message: error.message });
-  return res.status(500).json({ message: error.message });
-};
