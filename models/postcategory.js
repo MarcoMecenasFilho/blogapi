78e62d530d95e08e75193fc4fd7b63b80cb09164
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-03-14 10:19:18 -0300

    cria model de postcategory

---
+++ models/postcategory.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory',
+    {},
+    { timestamps: false, tableName: 'PostsCategories' });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      foreignKey: 'postId', 
+      otherKey: 'categoryId',
+      through: PostCategory,
+      as: 'categories',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      through: PostCategory,
+      as: 'blogPosts',
+    });
+  };
+
+  return PostCategory;
+};
